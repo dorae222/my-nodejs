a225db819a84871b81bb0b3c77fe8b9f5443debe
dorae222 <dhj9842@gmail.com>
2025-09-02 11:30:24 +0900

    Real Login Logic

--- webServer/ex02_router/page.js
+++ webServer/ex02_router/page.js
@@ -7,9 +7,16 @@ http.createServer(async(req,res)=>{
     // 서버 기본 주소 : https://localhost:3000
     // 회원가입 페이지 주소 : https://localhost:3000/join
     // 로그인 페이지 주소 : https://localhost:3000/login
+    // https://localhost:3000/user/login → 로그인 처리(입력한 id,pw 페이지 출력)
+
+    // 사용자가 입력한 값에 따라서 페이지에 출력값이 달라져야 함(html 가공)
+    // 1. 템플릿엔진 (ejs, pug, nunjucks 등) => 페이지 구성이 복잡할 때(일반적)
+    // 2. 서버쪽에서 html을 매번 생성해주는 방법 => 페이지 구성이 매우 단순할 때
 
     const parseUrl = url.parse(req.url,true);
+    console.log(parseUrl);
     const pathname = parseUrl.pathname; // '/join', '/login'
+    const query = parseUrl.query; // {id: 'sample', pw: 'sample_pw'}
 
     // pathname 값에 따라서
     // join => join.html | login => login.html
@@ -19,6 +26,16 @@ http.createServer(async(req,res)=>{
         file = await fs.readFile('./join.html');
     } else if (pathname === '/login') {
         file = await fs.readFile('./login.html');
+    } else if (pathname ==='/user/login'){
+        let html = '<html>';
+        html += '<body>';
+        html += '<h1>' + query.id + '</h1>';
+        html += '<h1>' + query.pw + '</h1>';
+        html += '</body>';
+        html += '</html>';
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(html);
+        res.end();
     }
 
     if (file){ // join or login 경로가 아닐 경우, file이 없음
